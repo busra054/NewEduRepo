8dec28d9e5562b9968a367f5ca8f713af94b6ff8
Büşra Önal <120249327+busra054@users.noreply.github.com>
2024-12-26 21:19:25 +0300

    firstComm

---
+++ EgitimUygulama/Models/Branch.cs
@@ -0,0 +1,11 @@
+﻿namespace EgitimUygulama.Models
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Relationship
+        public ICollection<TeacherBranch> TeacherBranches { get; set; }
+    }
+}
---
+++ EgitimUygulama/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EgitimUygulama.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
